ee8dd0c91b4bfc3cefde25a9c628d4e282d3cca7
emblafant <embla98wallen@gmail.com>
2021-10-20 16:44:41 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+//Buttons
+const submitButton = document.querySelector("#submit");
+const darkLightModeButton = document.querySelector("#darkLightMode");
+
+//Misc Elements
+const resultDiv = document.querySelector("#resultDiv");
+
+//Answer buttons
+const q1Buttons = document.querySelectorAll("[name='q1']");
+const q10Buttons = document.querySelectorAll("[name='q10']");
+
+
+//Quiz
+submitButton.addEventListener("click", () =>{
+  let questionsAnswered = 0;
+  let score = 0;
+
+
+  //Question 1
+  q1Buttons.forEach((button) => {
+    if (button.checked && button.value === "true") {
+      score++;
+    }
+    if (button.checked) {
+      questionsAnswered = +1;
+    }
+  });
+
+  //Question 10
+  let question10total = 0;
+  let question10answerd = false;
+  q10Buttons.forEach((button) => {
+    if (button.checked && button.value === "true") {
+      question10total++;
+      question10answerd = true;
+    };
+  });
+  if (question10total === 7) {
+    score++;
+  }
+  if (question10answerd) {
+    questionsAnswered++;
+  }
+});
+
+
+//Light and Dark mode
+let lightModeOn = true;
+darkLightModeButton.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  if (lightModeOn) {
+    body.style.background = "black";
+    body.style.color = "white";
+    lightModeOn = false;
+    darkLightModeButton.innerText = "Light-Mode";
+  } else {
+    body.style.background = "white";
+    body.style.color = "black";
+    lightModeOn = true;
+    darkLightModeButton.innerText = "Dark-Mode";
+  };
+});
+
+
+//Quiz resetbutton
